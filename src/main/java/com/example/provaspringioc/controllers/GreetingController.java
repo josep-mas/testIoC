9b55713420a003dd8cd9726efcdc87c687fe4568
josep-mas <jmasso@uoc.edu>
2023-08-12 12:43:42 +0200

    FIX: Controller

--- src/main/java/com/example/provaspringioc/controllers/GreetingController.java
+++ src/main/java/com/example/provaspringioc/controllers/GreetingController.java
@@ -1,4 +1,22 @@
 package com.example.provaspringioc.controllers;
 
+import com.example.provaspringioc.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingController {
+
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController( GreetingService greetingService ) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return greetingService.greet();
+    }
 }
--- src/main/java/com/example/provaspringioc/services/GreetingService.java
+++ src/main/java/com/example/provaspringioc/services/GreetingService.java
@@ -1,4 +1,11 @@
 package com.example.provaspringioc.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingService {
+
+    public String greet(){
+        return "Hello, Spring Boot!";
+    }
 }
